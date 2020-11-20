ef67381af8a15d73a4eb21fa27d667aa7bfb567c
kuchichan <pawel.kucharski@hotmail.com>
2020-11-20 21:19:53 +0100

    uniq - without modification of other commands

---
+++ exc_11/uniq.py
@@ -0,0 +1,17 @@
+def get_input():
+    while True:
+        try:
+            yield input()
+        except EOFError:
+            break
+
+
+if __name__ == "__main__":
+    stdin = get_input()
+    visited = []
+    for elem in stdin:
+        if elem not in visited:
+            print(elem)
+            visited.append(elem)
+    
+    
--- exc_8/cut.py
+++ exc_8/cut.py
@@ -14,23 +14,29 @@ def read_from_stdin(begin, end, delimiter):
     a = get_input()
 
     for line in a:
-        print(" ".join(line.split(delimiter)[begin:end]))
+        print(" ".join(line.split(delimiter)[begin-1:end]))
 
 def read_from_file(begin, end, delimiter, file_):
     with open(file_, 'r') as f:
         for line in f:
-            print(" ".join(line.split(delimiter)[begin:end]))
+            print(" ".join(line.split(delimiter)[begin-1:end]))
     
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser("find")
+    parser = argparse.ArgumentParser("cut")
     parser.add_argument("-d", type=str, default="\t")
     parser.add_argument("-f", type=str, default="")
-    parser.add_argument("file", type=str)
+    parser.add_argument("file", type=str, nargs="?", default="")
 
     args = parser.parse_args()
-    begin, end = [int(x) for x in args.f.split('-')]
-    if args.file is None:
+
+    range_ = [int(x) for x in args.f.split('-')]
+    try:
+        begin, end = range_
+    except ValueError:
+        begin = end = range_[0]
+    
+    if not args.file:
         read_from_stdin(begin, end, args.d)
     else:
         abspath = os.path.join(ROOT, args.file)
