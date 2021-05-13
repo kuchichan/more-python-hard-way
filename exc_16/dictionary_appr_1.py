1764f076ded68be68c72292a4ea23ca890e09768
kuchichan <pawel.kucharski@hotmail.com>
2021-05-13 22:07:25 +0200

    Repeated master copy completely from scratch

--- exc_16/dictionary_appr_1.py
+++ exc_16/dictionary_appr_1.py
@@ -2,16 +2,17 @@ from exc_13.dllist.dllist.double_linked_list import DoubleLinkedList
 
 class Dictionary(object):
     def __init__(self, num_buckets=256) -> None:
-        self.map = DoubleLinkedList()
+        self.map_ = DoubleLinkedList()
         for _ in range(num_buckets):
-            self.map.push(DoubleLinkedList())
+            self.map_.push(DoubleLinkedList())
 
-    def hash_key(self, key):
-        return hash(key) % self.map.count() 
+
+    def get_hash(self, key):
+        return hash(key) % self.map_.count()
 
     def get_bucket(self, key):
-        bucket_num = self.hash_key(key)
-        return self.map.get(bucket_num)
+        bucket_num = self.get_hash(key)
+        return self.map_.get(bucket_num)
 
     def get_slot(self, key, default=None):
         bucket = self.get_bucket(key)
@@ -20,45 +21,37 @@ class Dictionary(object):
 
             while node:
                 if node.value[0] == key:
-                    return bucket, node 
+                    return bucket, node
+                else:
+                    node = node.next_
 
-                node = node.next_
-
-        return bucket, None 
+        return bucket, None
 
     def get(self, key, default=None):
-        bucket, node = self.get_slot(key, default)
-        return node and node.value[1] or default 
+        bucket, slot = self.get_slot(key, default=default)
+        return slot and slot.value[1] or default
 
     def set(self, key, value):
         bucket, slot = self.get_slot(key)
-
         if slot:
             slot.value = (key, value)
-            
         else:
             bucket.push((key, value))
-
 
     def delete(self, key):
         bucket = self.get_bucket(key)
 
         node = bucket.begin
-
         while node:
             if node.value[0] == key:
                 bucket.detach(node)
                 break
-            node = node.next_
-
 
     def list(self):
-        map_begin = self.map.begin
+        map_begin = self.map_.begin
         while map_begin:
             bucket_begin = map_begin.value.begin
             while bucket_begin:
                 print(bucket_begin.value)
                 bucket_begin = bucket_begin.next_
             map_begin = map_begin.next_
-                
-    
