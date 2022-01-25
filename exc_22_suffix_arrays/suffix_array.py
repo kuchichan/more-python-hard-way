d40b0f97a5ae6425076f3c76af034a22b0a4c3d1
kuchichan <pawel.kucharski@hotmail.com>
2022-01-25 20:22:05 +0100

    Changes after comparison with zed shaw solutions

--- exc_22_suffix_arrays/suffix_array.py
+++ exc_22_suffix_arrays/suffix_array.py
@@ -3,51 +3,78 @@ from typing import List, Optional, Tuple
 
 class SuffixArray:
     def __init__(self, string_: str) -> None:
-        self._array: List[str] = []
+        self._sentence = string_
+        self._array: List[Tuple[str, int]] = []
 
         for i in range(0, len(string_)):
-            self._array.append(string_[i:])
+            self._array.append((string_[i:], i))
 
         self._array.sort()
 
     @property
-    def array(self) -> List[str]:
+    def array(self) -> List[Tuple[str, int]]:
         return self._array
 
-    def find_shortest(self, string_: str) -> Tuple[Optional[str], Optional[int]]:
-        found, found_index = None, None
+    def search(self, P: str) -> Tuple[int, int]:
+        """
+        Return indices (s, r) such that the interval A[s:r] (including the end
+        index) represents all suffixes of S that start with the pattern P.
+        """
+        # Find starting position of interval
+        l = 0  # in Python, arrays are indexed starting at 0
+        r = len(self._sentence)
+        while l < r:
+            mid = (l + r) // 2  # division rounding down to nearest integer
+            # suffixAt(A[i]) is the ith smallest suffix
+            if P > self.array[mid][0]:
+                l = mid + 1
+            else:
+                r = mid
+        s = l
+
+        # Find ending position of interval
+        r = len(self._sentence)
+        while l < r:
+            mid = (l + r) // 2
+            if self.array[mid][0].startswith(P):
+                l = mid + 1
+            else:
+                r = mid
+        return (s, r)
+
+    # I really do not understand what's going on ->
+    # This will found only the shortest, if its already in array
+    # Compared with Zed Shaw solution
+    def binary_search(self, string_: str) -> Tuple[int, int]:
         left = 0
-        right = len(self.array) - 1
+        right = len(self.array)
         mid = len(self.array) // 2
 
-        while left <= right:
-            mid_suffix = self.array[mid]
-            mid_index = mid
-            if string_ < mid_suffix:
-                right = mid - 1
-                mid = (right - left) // 2
-                if (not found or mid_suffix < found) and mid_suffix.startswith(string_):
-                    found = mid_suffix
-                    found_index = mid_index
-
-            elif string_ > mid_suffix:
+        while left < right:
+            mid = (right - left) // 2 + left
+            mid_val, starts_at = self.array[mid]
+            if mid_val == string_:
+                return mid, starts_at
+            if mid_val > string_:
+                right = mid
+            if mid_val < string_:
                 left = mid + 1
-                mid = (right + left) // 2
-                if (not found or mid_suffix < found) and mid_suffix.startswith(string_):
-                    found = mid_suffix
-                    found_index = mid_index
-            else:
-                return mid_suffix, mid_index
 
-        return found, found_index
+        return -1, -1
+
+    def find_shortest(self, _string):
+        mid, starts_at = self.binary_search(_string)
+        if mid < 0:
+            return -1, -1
+        return self._sentence[starts_at:], starts_at
 
     def find_longest(self, string_: str) -> Optional[str]:
         substring, index = self.find_shortest(string_)
 
-        if substring is None:
-            return None
+        if not substring:
+            return ""
 
-        for suffix in self.array[index:]:
+        for suffix, _ in self.array[index:]:
             if not suffix.startswith(string_):
                 break
             substring = suffix
